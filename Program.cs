67c4a9041787ca6bc939b6426fb9aa484087bd4a
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-09-27 19:38:15 +0300

    Created response

--- Program.cs
+++ Program.cs
@@ -1,6 +1,35 @@
 ﻿using Newtonsoft.Json;
 using System.Net.Http;
 
+for (int i = 0; i < 200; i += 20)
+{
+await getUsers(i);
+}
+async Task<int> getUsers(int offset)
+{
+using (HttpClient httpClient = new HttpClient())
+{
+string url = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
+HttpResponseMessage response = await httpClient.GetAsync(url);
+string content = await response.Content.ReadAsStringAsync();
+UsersResponse usersResponse = JsonConvert.DeserializeObject<UsersResponse>(content);
+var now = DateTime.Now;
+foreach (var user in usersResponse.data)
+{
+if (user.isOnline)
+{
+Console.WriteLine($"{user.nickname} is online");
+}
+else
+{
+Console.WriteLine($"{user.nickname} was last seen online {userStatus((DateTime)user.lastSeenDate)}");
+}
+}
+}
+return 1;
+}
+
+
 class UsersResponse
 {
     public int total;
