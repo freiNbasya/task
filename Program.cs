8c300c7a8b32192dbbe969bf2d085654ee3c5d3b
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-09-27 19:38:45 +0300

    Created lastSeenDate logic

--- Program.cs
+++ Program.cs
@@ -3,33 +3,49 @@ using System.Net.Http;
 
 for (int i = 0; i < 200; i += 20)
 {
-await getUsers(i);
+    await getUsers(i);
 }
 async Task<int> getUsers(int offset)
 {
-using (HttpClient httpClient = new HttpClient())
-{
-string url = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
-HttpResponseMessage response = await httpClient.GetAsync(url);
-string content = await response.Content.ReadAsStringAsync();
-UsersResponse usersResponse = JsonConvert.DeserializeObject<UsersResponse>(content);
-var now = DateTime.Now;
-foreach (var user in usersResponse.data)
-{
-if (user.isOnline)
-{
-Console.WriteLine($"{user.nickname} is online");
-}
-else
-{
-Console.WriteLine($"{user.nickname} was last seen online {userStatus((DateTime)user.lastSeenDate)}");
-}
-}
-}
-return 1;
+    using (HttpClient httpClient = new HttpClient())
+    {
+        string url = $"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}";
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+        string content = await response.Content.ReadAsStringAsync();
+        UsersResponse usersResponse = JsonConvert.DeserializeObject<UsersResponse>(content);
+        var now = DateTime.Now;
+        foreach (var user in usersResponse.data)
+        {
+            if (user.isOnline)
+            {
+                Console.WriteLine($"{user.nickname} is online");
+            }
+            else
+            {
+                Console.WriteLine($"{user.nickname} was last seen online {userStatus((DateTime)user.lastSeenDate)}");
+            }
+        }
+    }
+    return 1;
 }
 
+string userStatus(DateTime lastSeenDate)
+{
+    TimeSpan timeDif = DateTime.Now - lastSeenDate;
+    int daysDifference = timeDif.Days;
+    int hoursDifference = timeDif.Hours;
+    int minutesDifference = timeDif.Minutes;
+    int secondsDifference = timeDif.Seconds;
+    if (daysDifference > 7) return "long time ago";
+    if (daysDifference > 1) return "this week";
+    if (lastSeenDate.Day != DateTime.Now.Day) return "yesterday";
+    if (hoursDifference > 2) return "today";
+    if (hoursDifference > 1) return "hour ago";
+    if (minutesDifference > 1) return "couple of minutes ago";
+    if (secondsDifference > 30) return "less than a minute ago";
+    return "just now";
 
+}
 class UsersResponse
 {
     public int total;
