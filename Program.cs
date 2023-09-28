875555e3d173c0f51f01d4a90fa5711e9d91d4c3
shito.bulo@gmail.com <shito.bulo@gmail.com>
2023-09-29 00:00:54 +0300

    LST-6 added tests, added separate class for languages and UserStatus

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿using Newtonsoft.Json;
 using System.Net.Http;
+using System.Text;
+using task;
 
-for (int i = 0; i < 200; i += 20)
+Console.OutputEncoding = Encoding.UTF8;
+
+List<string> phrases = new List<string>();
+int total = 200;
+
+Console.WriteLine("Яку мову хочете обрати/What language you want to choose: 1 - Українська, 2 - English");
+int input;
+while (!int.TryParse(Console.ReadLine(), out input) || (input != 1 && input != 2))
+{
+    Console.WriteLine("Невірний ввід/Invalid input. Введіть 1 або 2/Enter 1 or 2.");
+}
+_ = input == 1 ? phrases = Languages.Ukrainian : phrases = Languages.English;
+
+for (int i = 0; i < total; i += 20)
 {
     await getUsers(i);
 }
@@ -13,45 +28,47 @@ async Task<int> getUsers(int offset)
         HttpResponseMessage response = await httpClient.GetAsync(url);
         string content = await response.Content.ReadAsStringAsync();
         UsersResponse usersResponse = JsonConvert.DeserializeObject<UsersResponse>(content);
+        total = usersResponse.total;
         var now = DateTime.Now;
         foreach (var user in usersResponse.data)
         {
-            if (user.isOnline)
+            if (user.isOnline )
             {
-                Console.WriteLine($"{user.nickname} is online");
+                Console.WriteLine($"{user.nickname} {phrases[0]}");
             }
             else
             {
-                Console.WriteLine($"{user.nickname} was last seen online {userStatus((DateTime)user.lastSeenDate)}");
+                Console.WriteLine($"{user.nickname} {phrases[1]} {UserStatus.GetStatus((DateTime)user.lastSeenDate, phrases)}");
             }
         }
     }
     return 1;
 }
 
-string userStatus(DateTime lastSeenDate)
-{
-    TimeSpan timeDif = DateTime.Now - lastSeenDate;
-    int daysDifference = timeDif.Days;
-    int hoursDifference = timeDif.Hours;
-    int minutesDifference = timeDif.Minutes;
-    int secondsDifference = timeDif.Seconds;
-    if (daysDifference > 7) return "long time ago";
-    if (daysDifference > 1) return "this week";
-    if (lastSeenDate.Day != DateTime.Now.Day) return "yesterday";
-    if (hoursDifference > 2) return "today";
-    if (hoursDifference > 1) return "hour ago";
-    if (minutesDifference > 1) return "couple of minutes ago";
-    if (secondsDifference > 30) return "less than a minute ago";
-    return "just now";
+//string userStatus(DateTime lastSeenDate)
+//{
+//    TimeSpan timeDif = DateTime.Now - lastSeenDate;
+//    int daysDifference = timeDif.Days;
+//    int hoursDifference = timeDif.Hours;
+//    int minutesDifference = timeDif.Minutes;
+//    int secondsDifference = timeDif.Seconds;
+//    if (daysDifference > 7 ) return phrases[2];
+//    if (daysDifference > 1 ) return phrases[3];
+//    if (lastSeenDate.Day != DateTime.Now.Day ) return phrases[4];
+//    if (hoursDifference > 2 ) return phrases[5];
+//    if (hoursDifference > 1 ) return phrases[6];
+//    if (minutesDifference > 1 ) return phrases[7];
+//    if (secondsDifference > 30 ) return phrases[8];
+//    return phrases[9];
 
-}
-class UsersResponse
+//}
+
+public class UsersResponse
 {
     public int total;
     public List<LastSeenUsers> data;
 };
-class LastSeenUsers
+public class LastSeenUsers
 {
     public string userId;
     public string nickname;
